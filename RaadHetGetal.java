425343486cae92e113c98ad451a01953bbada684
Rikkertz <132992098+Rikkertz@users.noreply.github.com>
2023-05-16 10:38:27 +0200

    Assignment complete

--- RaadHetGetal.java
+++ RaadHetGetal.java
@@ -1,28 +1,49 @@
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 import static java.lang.Integer.parseInt;
 
 public class RaadHetGetal {
-    public static void main( String args[] ) {
-        //  Maak een getal tussen 0 en 1000
+    public static void main(String[] args) {
         Random rand = new Random();
-        //  Genereer een getal tussen 1 en 999 inclusief
+        //  Generate a number between 1 and 1000
         int number = rand.nextInt(999) + 1;
-
-        while(true){
+        int guesses = 0;
+        // User has 10 guesses to guess the number
+        while(guesses < 10){
             Scanner userinput = new Scanner(System.in);
-            System.out.println("Raad een getal tussen 1 en 1000");
+            System.out.printf("Guess a number between 1 and 1000.\nYou have %s guesses left.\n", (10-guesses));
 
             int guess = parseInt(userinput.nextLine());
             if(guess == number){
-                System.out.println("Congratulations!");
-                break;
+                System.out.printf("Congratulations! The number was %s.\n", number);
+                // Allow the user to play again without restarting the program
+                System.out.println("Type 'Yes' if you want to play again.");
+                String playagain = userinput.nextLine();
+                if(Objects.equals(playagain, "Yes")){
+                    number = rand.nextInt(999) + 1;
+                    guesses = 0;
+                    continue;
+                }
+                return;
             } else if (guess <= number) {
                 System.out.println("Your guess was too low!");
             } else {
                 System.out.println("Your guess was too high!");
             }
+            guesses ++;
+            if(guesses == 10){
+            System.out.printf("You have run out of guesses. The number was %s\n", number);
+            // Allow the user to play again without restarting the program
+            System.out.println("Type 'Yes' if you want to play again.");
+            String playagain = userinput.nextLine();
+            if(Objects.equals(playagain, "Yes")) {
+                number = rand.nextInt(999) + 1;
+                guesses = 0;
             }
+            }
+            }
+
 
         }
 }
